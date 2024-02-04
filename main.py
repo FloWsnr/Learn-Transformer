c8f3bddb27447ab3bedd77e413b9ffc4231323e5
Florian Wiesner <wiesnerflorian94@gmail.com>
2024-02-04 20:47:25 +0100

    attention and most other layers required for the transformer

--- main.py
+++ main.py
@@ -4,45 +4,127 @@ import torch
 class TransformerLayer(torch.nn.Module):
     def __init__(self):
         super(TransformerLayer, self).__init__()
-        self.mlp = torch.nn.Sequential(
-            torch.nn.Linear(512, 2048),
-            torch.nn.ReLU(),
-            torch.nn.Linear(2048, 512),
-        )
+        self.multi_head_attention = MultiHeadAttention()
+        self.feed_forward = FeedForward()
 
-        self.q = torch.nn.Linear(512, 100)
-        self.k = torch.nn.Linear(512, 100)
-        self.v = torch.nn.Linear(512, 100)
+    def forward(self, x: torch.Tensor):
+        # x: [batch, 512]
 
-    def forward(self, x):
-        # Multi-head attention
-        q = self.q(x)
-        k = self.k(x)
-        v = self.v(x)
-
-        # calc attention
-        attention = torch.matmul(q, k.T)
-        attention = attention / torch.sqrt(torch.tensor(100.0))
-        attention = torch.softmax(attention, dim=-1)
-        attention = torch.matmul(attention, v)
-
-        # Normalization
-        x = x + attention  # residual
-        x = torch.nn.LayerNorm(x)
+        # Multi-head attentiion
+        x = self.multi_head_attention(x)
 
         # Feed forward
-        x_new = self.mlp(x)
+        x = self.feed_forward(x)
+
+        return x
+
+
+class FeedForward(torch.nn.Module):
+    def __init__(self, input_dim: int = 512, hidden_dim: int = 2048):
+        super(FeedForward, self).__init__()
+        self.mlp = torch.nn.Sequential(
+            torch.nn.Linear(input_dim, hidden_dim),
+            torch.nn.ReLU(),
+            torch.nn.Linear(hidden_dim, input_dim),
+        )
+        self.layer_norm = torch.nn.LayerNorm(input_dim)
+
+    def forward(self, x: torch.Tensor):
+        x = self.mlp(x) + x  # + residual (skip connection)
+        x = self.layer_norm(x)
+        return x
+
+
+class MultiHeadAttention(torch.nn.Module):
+    def __init__(self, input_dim: int = 512, num_heads: int = 8):
+        super(MultiHeadAttention, self).__init__()
+
+        self.Wq = torch.nn.Linear(input_dim, input_dim)
+        self.Wk = torch.nn.Linear(input_dim, input_dim)
+        self.Wv = torch.nn.Linear(input_dim, input_dim)
+        self.layer_norm = torch.nn.LayerNorm(input_dim)
+
+        self.num_heads = num_heads
+
+        # Check if input_dim is divisible by num_heads
+        if input_dim % num_heads != 0:
+            raise ValueError(
+                f"input_dim ({input_dim}) should be divisible by num_heads ({num_heads})"
+            )
+        self.head_dim = input_dim // num_heads
+
+    def forward(self, x: torch.Tensor):
+
+        # Multi-head attention
+        q: torch.Tensor = self.Wq(x)
+        k: torch.Tensor = self.Wk(x)
+        v: torch.Tensor = self.Wv(x)
+
+        # Split heads
+        q = q.view(-1, self.num_heads, self.head_dim)
+        k = k.view(-1, self.num_heads, self.head_dim)
+        v = v.view(-1, self.num_heads, self.head_dim)
+
+        d_k = torch.tensor(k.shape[-1])
+
+        # calc attention
+        score = torch.matmul(q, k.transpose(-1, -2))
+        score = score / torch.sqrt(d_k)
+        attention = torch.softmax(score, dim=-1)
+        context = torch.matmul(attention, v)
+
+        # Concatenate heads
+        context = context.view(-1, 512)
 
         # Normalization
-        x = x_new + x  # residual
-        x = torch.nn.LayerNorm(x)
+        x = x + context  # residual
+        x = self.layer_norm(x)
 
         return x
 
 
-class AttentionHead(torch.nn.Module):
-    def __init__(self):
-        super(AttentionHead, self).__init__()
+class Embedding(torch.nn.Module):
+    """Convert list of indices to list of vectors using embedding layer.
 
-    def forward(self, x):
+    The indices are the enumeration of the vocabulary. For example,
+    if the vocabulary is ["The","apple", "is", "delicious"], then the indices
+    are [0, 1, 2, 3].
+
+    If the sentence is "delicious apple", then the input is [3, 1].
+
+    Parameters
+    ----------
+    num_embeddings : int
+        The size of the vocabulary, by default 512.
+
+    dim_embeddings : int
+        The dimension of the embedding vectors.
+        That is, each word is represented as a vector of dim_embeddings.
+        By default 512.
+
+    Returns
+    -------
+    x : torch.Tensor
+        List of embedding vectors.
+
+    Notes
+    -----
+    The embedding layer is a simple linear layer.
+    In the "Attention is All You Need" paper, the embedding layer is shared by
+    the encoder and decoder. That is, the same embedding layer is used.
+    """
+
+    def __init__(self, num_embeddings: int = 512, dim_embeddings: int = 512) -> None:
+        super(Embedding, self).__init__()
+        self.embedding = torch.nn.Embedding(num_embeddings, dim_embeddings)
+
+    def forward(self, indices: torch.Tensor) -> torch.Tensor:
+        x = self.embedding(indices)
         return x
+
+
+if __name__ == "__main__":
+    layer = TransformerLayer()
+    x = torch.randn(10, 512)
+    out = layer(x)
+    print(out)
