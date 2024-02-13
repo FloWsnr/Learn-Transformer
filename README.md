096d97f6b8e580209a8e4f9911cd1c3c82633cb4
Florian Wiesner <wiesnerflorian94@gmail.com>
2024-02-13 16:16:46 +0100

    own Tokenizer pipeline using hugging face tokenizer

--- README.md
+++ README.md
@@ -5,7 +5,6 @@
 ```bash
 conda install pytorch torchtext pytorch-cuda=12.1 -c pytorch -c nvidia
 conda install pytest
-conda install -c conda-forge tiktoken
-pip install datasets
+pip install datasets, tokenizers
 pip install -e .
 ```
--- learn_former/data/dataset.py
+++ learn_former/data/dataset.py
@@ -3,11 +3,16 @@ from pathlib import Path
 from torch.utils.data import DataLoader
 from datasets import load_dataset, DatasetDict
 
+from tokenizers import Tokenizer
+from tokenizers.models import BPE
+from tokenizers.trainers import BpeTrainer
+from tokenizers.pre_tokenizers import Whitespace
+
 
 def get_dataset(
     storage_dir: Path,
     dataset_name: str = "wmt16",
-):
+) -> DatasetDict:
     dataset: DatasetDict = load_dataset(
         dataset_name, name="de-en", cache_dir=storage_dir
     )
@@ -15,12 +20,10 @@ def get_dataset(
 
 
 def get_dataloaders(
-    storage_dir: Path,
-    dataset_name: str = "wmt16",
+    dataset: DatasetDict,
     batch_size: int = 32,
     num_workers: int = 4,
-):
-    dataset = get_dataset(storage_dir, dataset_name)
+) -> tuple[DataLoader, DataLoader, DataLoader]:
     train_loader = DataLoader(
         dataset["train"], batch_size=batch_size, num_workers=num_workers
     )
@@ -31,3 +34,51 @@ def get_dataloaders(
         dataset["test"], batch_size=batch_size, num_workers=num_workers
     )
     return train_loader, val_loader, test_loader
+
+
+class Tokenizer_Pipeline:
+    def __init__(self, storage_dir: Path, dataset_name: str = "wmt16"):
+        self.tokenizer = None
+
+        if (storage_dir / "tokenizer.json").exists():
+            self.tokenizer = self.load_tokenizer(storage_dir)
+            print("Tokenizer loaded from storage")
+        else:
+            self.dataset = get_dataset(storage_dir, dataset_name)
+            self.tokenizer = self.train_tokenizer()
+            self.save_tokenizer(self.tokenizer, storage_dir)
+            print("Tokenizer trained and saved")
+
+        self.tokenizer.enable_padding(pad_id=3, pad_token="[PAD]")
+
+    def train_tokenizer(self, vocab_size: int = 32000) -> Tokenizer:
+        tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
+        trainer = BpeTrainer(special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]"])
+
+        tokenizer.pre_tokenizer = Whitespace()
+
+        tokenizer.train_from_iterator(
+            self.dataset["train"]["translation.de"],
+            trainer=trainer,
+            vocab_size=vocab_size,
+        )
+        return tokenizer
+
+    def save_tokenizer(self, storage_dir: Path) -> None:
+        self.tokenizer.save(storage_dir / "tokenizer.json")
+
+    def load_tokenizer(self, storage_dir: Path) -> Tokenizer:
+        tokenizer = Tokenizer.from_file(storage_dir / "tokenizer.json")
+        return tokenizer
+
+    def encode_batch(self, text_batch: list[str]) -> list[int]:
+        output = self.tokenizer.encode_batch(text_batch)
+
+        token_ids = [x.ids for x in output]
+        attention_masks = [x.attention_mask for x in output]
+
+        return token_ids, attention_masks
+
+    def decode_batch(self, token_ids: list[int]) -> list[str]:
+        output = self.tokenizer.decode_batch(token_ids)
+        return output
