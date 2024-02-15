5652f572b1af593a24e4f70ef6acfd0804cd6c78
Florian Wiesner <wiesnerflorian94@gmail.com>
2024-02-15 16:48:15 +0100

    test for using dataloader in loop

--- tests/test_data/test_dataset.py
+++ tests/test_data/test_dataset.py
@@ -49,3 +49,21 @@ def test_get_dataloaders_batch(learn_former_root_dir: Path):
     y = x_train["en"]
 
     assert isinstance(x, list)
+
+
+def test_dataloader_for_loop(learn_former_root_dir: Path):
+
+    dataset_dir = learn_former_root_dir / "data/datasets"
+    dataset = get_dataset(storage_dir=dataset_dir, dataset_name="wmt16")
+
+    dataloaders = get_dataloaders(dataset, batch_size=1, num_workers=0)
+    train_loader, val_loader, test_loader = dataloaders
+
+    for batch in val_loader:
+        x = batch["de"]
+        y = batch["en"]
+
+        assert isinstance(x, list)
+        assert isinstance(y, list)
+
+        break
