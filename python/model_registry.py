9babf6aa769b65d52012a42b2e040a018b918df6
Florian Wiesner <wiesnerflorian94@gmail.com>
2026-02-07 21:03:20 +0100

    finish registry test

--- python/model_registry.py
+++ python/model_registry.py
@@ -3,6 +3,12 @@ from typing import Optional
 from datetime import datetime
 
 
+def get_version(version: str) -> tuple[int, ...]:
+    nums = version.split(".")
+    num_version = tuple([int(n) for n in nums])
+    return num_version
+
+
 @dataclass
 class Model:
     name: str
@@ -13,25 +19,70 @@ class Model:
 
     def __gt__(self, other: "Model") -> bool:
         """Compare by semantic version."""
-        ...
+        self_v = get_version(self.version)
+        other_v = get_version(other.version)
+
+        return self_v > other_v
+
+    def __eq__(self, other: "Model") -> bool:
+        """Compare by semantic version."""
+        if self.name != other.name:
+            return False
+
+        self_v = get_version(self.version)
+        other_v = get_version(other.version)
+        return self_v == other_v
 
 
 class ModelRegistry:
-    def __init__(self): ...
+    def __init__(self):
+        self._models: list[Model] = []
 
     def register(self, model: Model) -> None:
         """Register a model. Raise if name+version already exists."""
-        ...
+        for ex_model in self._models:
+            if model == ex_model:
+                raise KeyError
+        self._models.append(model)
 
     def get_latest(self, name: str) -> Optional[Model]:
         """Return the highest version of a model by name."""
-        ...
+        highest = None
+        for ex_model in self._models:
+            if name == ex_model.name:
+                if highest is None:
+                    highest = ex_model
+                elif ex_model > highest:
+                    highest = ex_model
+
+        return highest
 
     def search(
         self, tag: Optional[str] = None, min_size: Optional[float] = None
     ) -> list[Model]:
         """Filter models by tag and/or minimum size."""
-        ...
+        models = self._models
+        if tag is not None:
+            models = self._search_tag(models, tag)
+        if min_size is not None:
+            models = self._search_size(models, min_size)
+        return models
+
+    @staticmethod
+    def _search_tag(models: list[Model], tag: str) -> list[Model]:
+        results = []
+        for ex_model in models:
+            if tag in ex_model.tags:
+                results.append(ex_model)
+        return results
+
+    @staticmethod
+    def _search_size(models: list[Model], min_size: float) -> list[Model]:
+        results = []
+        for ex_model in models:
+            if ex_model.size_gb >= min_size:
+                results.append(ex_model)
+        return results
 
 
 # Usage:
