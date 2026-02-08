0b06f30ad543f80c468a5bb404e0ac52869a42e3
Florian Wiesner <wiesnerflorian94@gmail.com>
2026-02-08 21:36:15 +0100

    add type hints and implement batch function in itertools.py

--- python/itertools.py
+++ python/itertools.py
@@ -1,17 +1,21 @@
 import itertools
 import functools
+from typing import Generator
 
 
-def batch(iterable, size: int):
+def batch(iterable, size: int) -> Generator[tuple]:
     """Yield successive chunks of `size` from any iterable.
     Last batch may be shorter. Must work with generators (no len())."""
-    ...
+
+    batched = itertools.batched(iterable, n=size)
+    for b in batched:
+        yield b
 
 
 def sliding_window(iterable, window_size: int):
     """Yield overlapping tuples of `window_size` from iterable.
     e.g. sliding_window([1,2,3,4,5], 3) -> (1,2,3), (2,3,4), (3,4,5)"""
-    ...
+    slices = itertools.islice(iterable)
 
 
 @functools.lru_cache(maxsize=128)
